3d9c5e71dcd5fef0eb08eab228af1f5ae53da9ee
pfg <omegariisthebest@gmail.com>
2019-05-18 07:26:09 -0700

    Try it button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
+
+
+// Replace pre blocks with code editor blocks
+Array.from(document.getElementsByClassName("language-scpleditor")).forEach(elem => {
+	let preblock = elem.parentNode
+	let container = preblock.parentNode;
+	
+	// create code editor block
+	let editor = document.createElement("div");
+	editor.classList.add("code");
+	let textarea = document.createElement("textarea");
+	textarea.setAttribute("autosize", true);
+	textarea.setAttribute("rows", 8);
+	textarea.value = elem.textContent;
+	editor.appendChild(textarea);
+	let enableJavascriptOverlay = document.createElement("div");
+	enableJavascriptOverlay.classList.add("enablejavascript");
+	enableJavascriptOverlay.textContent = "Enable javascript to edit this text";
+	editor.appendChild(enableJavascriptOverlay);
+	let tryitbtn = document.createElement("a");
+	tryitbtn.classList.add("tryitbtn");
+	tryitbtn.setAttribute("href", "https://editor.scpl.dev/?scpl="+encodeURIComponent(textarea.value));
+	tryitbtn.textContent = "Try It!";
+	editor.appendChild(tryitbtn);
+	
+	container.replaceChild(editor, preblock);
+});
+
+// Autosizing for code editor blocks
 function autosize(t){t.setAttribute("rows", Math.max(t.value.split(`\n`).length, 5));}
 
 Array.from(document.getElementsByClassName("code")).forEach(el => {
